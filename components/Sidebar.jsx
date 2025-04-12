7f64ded5838a5f356615d138e9d7540a77fd830b
Abubakr Sadriddinov <abubakrsadriddinov@eduroam-226-87-56.laptops.usf.edu>
2025-04-12 18:13:51 -0400

    initial layout

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { ArrowRightFromLine } from "lucide-react";
+import ProfileCircle from "./ProfileCircle";
 
 const Sidebar = () => {
   const [sidebarExpanded, setSidebarExpanded] = useState(false);
@@ -11,13 +12,19 @@ const Sidebar = () => {
 
   return (
     <div
-      className={`flex flex-col transition-all duration-300 h-[calc(100vh-64px)] bg-amber-300 ${
-        sidebarExpanded ? "w-64" : "w-24"
+      className={`flex flex-col items-center transition-all duration-300 h-[calc(100vh-64px)] bg-amber-300 ${
+        sidebarExpanded ? "w-64" : "w-20"
       }`}
     >
-      <button onClick={toggleSidebar} className="p-2">
-        <ArrowRightFromLine />
+      <button onClick={toggleSidebar} className="p-2 mt-4">
+        <ArrowRightFromLine size={30} />
       </button>
+
+      <div className="flex-grow"></div>
+
+      <div className="mb-4">
+        <ProfileCircle />
+      </div>
     </div>
   );
 };
