a8ba94a2de9fcae811252a339704d20b7b33a549
Abubakr Sadriddinov <abubakrsadriddinov@eduroam-226-87-56.laptops.usf.edu>
2025-04-12 18:00:27 -0400

    sidebar updated

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,9 +1,25 @@
+"use client";
+import { useState } from "react";
+import { ArrowRightFromLine } from "lucide-react";
+
 const Sidebar = () => {
-    return ( 
-        <div className="flex flex-col w-24 h-[calc(100vh-64px)] bg-amber-300">
-            <button></button>
-        </div>
-     );
-}
- 
+  const [sidebarExpanded, setSidebarExpanded] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarExpanded((prev) => !prev);
+  };
+
+  return (
+    <div
+      className={`flex flex-col transition-all duration-300 h-[calc(100vh-64px)] bg-amber-300 ${
+        sidebarExpanded ? "w-64" : "w-24"
+      }`}
+    >
+      <button onClick={toggleSidebar} className="p-2">
+        <ArrowRightFromLine />
+      </button>
+    </div>
+  );
+};
+
 export default Sidebar;
